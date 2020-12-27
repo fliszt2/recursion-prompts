e5ebc48ce157efaa7c9ef2ecbf17d7558deac0a8
fliszt2 <rkelly2012@gmail.com>
2020-12-27 14:54:21 -0600

    Implemented Problem 10

--- src/recursion.js
+++ src/recursion.js
@@ -133,6 +133,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase();
+  if (string.length === 1) {
+    return true;
+  }
+  if (string[0] !== string.slice(-1)) {
+    return false;
+  }
+  return palindrome(string.slice(1, -1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
